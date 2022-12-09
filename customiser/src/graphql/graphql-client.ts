ef225eb7d2abd88ff8ef36497985d7717b02f43e
Marcus Williams <marcus.williams@joipolloi.com>
2022-12-09 15:34:52 +0000

    Remove exposed token

--- customiser/src/graphql/graphql-client.ts
+++ customiser/src/graphql/graphql-client.ts
@@ -2,19 +2,9 @@ import { QueryClient } from '@tanstack/react-query';
 import { GraphQLClient } from 'graphql-request';
 import { EnvVars } from './env';
 
-export const endpoint = EnvVars.API_URL as string;
-export const fetchParams = {
-  headers: {
-    Authorization: `Bearer ${EnvVars.API_TOKEN as string}`,
-    'Content-Type': 'application/json',
-  },
-};
+export const endpoint = process.env.REACT_APP_API_URL as string;
 
-export const graphQLClient = new GraphQLClient(endpoint, {
-  headers: {
-    Authorization: `Bearer ${EnvVars.API_TOKEN as string}`,
-  },
-});
+export const graphQLClient = new GraphQLClient(endpoint);
 
 export const endpointShopify = EnvVars.STOREFRONT_URL as string;
 export const fetchParamsShopify = {

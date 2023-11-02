9617959ac16c954e86d7aba4f93cbd899d3defea
naierii <reianviloria@gmail.com>
2023-11-02 22:25:43 +0800

    #4 removed test canvas and enhanced the canvas class

--- customiser/src/components/nav/NavText/NavTextPreview.tsx
+++ customiser/src/components/nav/NavText/NavTextPreview.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import type { NavTextSelectProps } from './NavTextSelect';
-import { Canvas2d } from '@lib/canvas';
+import { CanvasText } from '@lib/canvas';
 
 // TODO - Pre load the fonts somewhere
 async function loadFonts(fontUrl: string) {
@@ -13,48 +13,36 @@ const NavTextPreview = ({ editText }: NavTextSelectProps) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const outlineCanvasRef = useRef<HTMLCanvasElement>(null);
   const [fontFamily, setFontFamily] = useState<string | undefined>(editText?.font);
-  console.log('editText', editText)
   
   useEffect(() => {
     const canvas = canvasRef.current as HTMLCanvasElement;
-    const canvas2d = new Canvas2d(canvas);
-    const ctx = canvas2d.getCtx();
-
     const outlineCanvas = outlineCanvasRef.current as HTMLCanvasElement;
-    const outlineCanvas2d = new Canvas2d(outlineCanvas);
-    const outlineCtx = outlineCanvas2d.getCtx();
     
-    const canvasWidth = canvas.width;
-    const canvasHeight = canvas.height;
-
     (async()=>{
+      const canvasText = new CanvasText(canvas, outlineCanvas);
+
       if (!editText || !editText.text) {
-        canvas2d.clear();
-        outlineCanvas2d.clear();
+        canvasText.clear();
         return;
       };
 
+      // Adjust this once preloading fonts has been implemented
       if (fontFamily !== editText?.font) {
         setFontFamily(editText?.font);
         await loadFonts(editText?.font as string);
       }
 
-      canvas2d.clear();
-      outlineCanvas2d.clear();
-
-      canvas2d.drawPreviewText(editText.text);
-      outlineCanvas2d.drawPreviewOutlineText(editText.text);
-
-      await canvas2d.maskMaterialImg(editText.material);
-      await outlineCanvas2d.maskMaterialImg(editText.outline);
-
-      outlineCtx.drawImage(canvas, 0, 0);
+      await canvasText.previewText({
+        text: editText.text,
+        material: editText.material,
+        outline: editText.outline,
+      });
     })()
   }, [editText]); // TO IMPROVE? - update only on certain value changes
 
   return (
     <div>
-      <canvas height={200} style={{width: '100%'}} ref={canvasRef}/>
+      <canvas height={200} style={{width: '100%', display: 'none'}} ref={canvasRef}/>
       <canvas height={200} style={{width: '100%'}} ref={outlineCanvasRef}/>
     </div>
   );
--- customiser/src/lib/canvas.ts
+++ customiser/src/lib/canvas.ts
@@ -10,43 +10,52 @@ async function loadImage(src: string): Promise<HTMLImageElement> {
   });
 }
 
-export class Canvas2d {
+function getMaterialUrl(material: MaterialFragment | undefined): string {
+  return material
+  ?.attributes
+  ?.images
+  ?.find(imgMap => imgMap?.mapType === 'map')
+  ?.image
+  .data
+  ?.attributes
+  ?.formats
+  .large
+  .url;
+}
+
+interface PreviewText {
+  text: string;
+  material: MaterialFragment | undefined;
+  outline: MaterialFragment | undefined;
+}
+
+export class CanvasText {
   canvas: HTMLCanvasElement;
   ctx: CanvasRenderingContext2D;
+  outlineCanvas: HTMLCanvasElement;
+  outlineCtx: CanvasRenderingContext2D;
 
-  constructor(canvas: HTMLCanvasElement) {
+  constructor(canvas: HTMLCanvasElement, outlineCanvas: HTMLCanvasElement) {
     this.canvas = canvas;
     this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
+    this.outlineCanvas = outlineCanvas;
+    this.outlineCtx = this.outlineCanvas.getContext('2d') as CanvasRenderingContext2D;
   }
 
   public clear(){
     this.ctx?.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.outlineCtx?.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
-
-  public async drawImage(src: string){
+  public async drawImg(ctx: CanvasRenderingContext2D, src: string) {
     const baseImage = await loadImage(src);
-    this.ctx?.drawImage(baseImage, 0, 0, this.canvas.width, this.canvas.height);
+    ctx.drawImage(baseImage, 0, 0, this.canvas.width, this.canvas.height);
   }
 
-  public async drawMaterialImg(material: MaterialFragment | undefined) {
-    const materialImgSrc = material
-      ?.attributes
-      ?.images
-      ?.find(imgMap => imgMap?.mapType === 'map')
-      ?.image
-      .data
-      ?.attributes
-      ?.formats
-      .large
-      .url;
-
-    await this.drawImage(materialImgSrc);
-  }
-
-  public async maskMaterialImg(material: MaterialFragment | undefined) {
-    this.ctx.globalCompositeOperation = 'source-in';
-    await this.drawMaterialImg(material);
-    this.ctx.globalCompositeOperation = 'source-over'; // reset to default
+  public async maskImage(ctx: CanvasRenderingContext2D, material: MaterialFragment | undefined) {
+    const src = getMaterialUrl(material);
+    ctx.globalCompositeOperation = 'source-in';
+    await this.drawImg(ctx, src);
+    ctx.globalCompositeOperation = 'source-over'; // reset to default
   }
 
   public drawPreviewText(text: string) {
@@ -56,16 +65,42 @@ export class Canvas2d {
   }
 
   public drawPreviewOutlineText(text: string) {
-    this.ctx.font = '45px testFont';
-    this.ctx.textAlign = 'center';
-    this.ctx.lineWidth = 4;
-    this.ctx.strokeText(text, this.canvas.width/2, this.canvas.height/2);
+    this.outlineCtx.font = '45px testFont';
+    this.outlineCtx.textAlign = 'center';
+    this.outlineCtx.lineWidth = 4;
+    this.outlineCtx.strokeText(text, this.canvas.width/2, this.canvas.height/2);
+  }
+
+  public async previewText({text, material, outline}: PreviewText) {
+    this.clear();
+    
+    if (outline) {
+      this.drawPreviewOutlineText(text);
+      await this.maskImage(this.outlineCtx, outline);
+    }
+
+    this.drawPreviewText(text);
+    await this.maskImage(this.ctx, material);
+    
+    this.outlineCtx.drawImage(this.canvas, 0, 0);
+  }
+
+  public mergeCanvas() {
+    this.outlineCtx.drawImage(this.canvas, 0, 0);
   }
 
   getCanvas(){
     return this.canvas;
   }
+  getOutlineCanvas(){
+    return this.outlineCanvas;
+  }
+
   getCtx(){
     return this.ctx;
   }
+  getOutlineCtx(){
+    return this.outlineCtx;
+  }
+
 }
